a7467a24da414b29668bdd17417358b58378b431
sang.ho <sang.ho25@student.passerellesnumeriques.org>
2024-01-30 19:43:10 +0700

    add the third  lesson for Laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,16 @@
-<h1> Welcome to Learning_Laravel at My Website</h1>
+<h1>Welcome to Learning_Laravel at My Website</h1>
+
+<?php
+// Output the value of APP_ENV using env() function
+echo env('APP_ENV');
+
+// Output the value of APP_ENV using config() function
+echo config('app.env');
+
+// Check if the environment is production
+if (env('APP_ENV') == 'production') {
+    echo 'call api live';
+} else {
+    echo 'call api sandbox';
+}
+?>
