226bda5a76dd7f87a5a1965a33cef1cda8255d2c
sang.ho <sang.ho25@student.passerellesnumeriques.org>
2024-01-31 06:17:57 +0700

    Laravel-Lesson4/add router in laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,15 +13,81 @@ use App\Models\User;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+  $html = '<h1>Welcome to My Website</h1>';
+  return $html;
 });
-Route::get('/laravel', function () {
-  $user = new User();
-  $allUser = $user::all();
-  dd($allUser);
-  // return view('home');
+
+/*
+Route::get('laravel', function(){
+   return "Phương thức get của path laravel";
 });
-Route::get('/product', function () {
-   return view('product');
+
+
+Route::get('laravel', function(){
+   return view('form');
+});
+
+
+Route::post('/laravel', function(){
+    return "Phương thức post của path laravel";
+});
+
+Route::put('/laravel', function(){
+    return "Phương thức put của path laravel";
+});
+
+Route::delete('/laravel', function(){
+    return "Phương thức delete của path laravel";
+});
+
+Route::patch('/laravel', function(){
+    return "Phương thức patch của path laravel";
+});
+
+Route::match(['get', 'post'], '/laravel', function(){
+  return $_SERVER['REQUEST_METHOD'];
+});
+
+
+
+Route::any('/laravel', function(Request $request){
+  return $request->method();
+});
+
+
+
+Route::get('show-form',function(){
+  return view('form');
+});
+
+Route::redirect('/laravel', 'show-form', 301);
+
+Route::view('show-form','form');
+*/
+
+Route::prefix('admin')->group(function () {
+  Route::get('/laravel', function () {
+    return "Phương thức get của path laravel";
+  });
+
+
+  Route::get('/show-form', function () {
+    return view('form');
+  });
+
+  Route::prefix('/products')->group(function () {
+    Route::get('/', function () {
+      return 'Danh sách sản phẩm';
+    });
+
+     Route::get('/add', function () {
+      return 'Thêm sản phẩm';
+    });
+
+     Route::get('/edit', function () {
+      return 'Sửa sản phẩm';
+    });
+  });
 });
