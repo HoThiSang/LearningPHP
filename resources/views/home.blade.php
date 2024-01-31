5d690eb1e16c2ced342fd06f604a9199409f2c30
sang.ho <sang.ho25@student.passerellesnumeriques.org>
2024-01-31 11:30:41 +0700

    Laravel-Lesson5: add router in laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 <h1>Welcome to Learning_Laravel at My Website</h1>
-
+<a href="<?php echo route('admin.show-form') ?>">Show form</a>
+<a href="<?php echo route('admin.products.add') ?>">Thêm sản phẩm</a>
+<a href="<?php echo route('admin.laravel', ['id'=>1, 'slug'=>'tin-tuc-the-gioi']) ?>">tin tức</a>
 <?php
 // Output the value of APP_ENV using env() function
 echo env('APP_ENV');
