26a0436dd3fafacfd8f095115010c8120471650f
sang.ho <sang.ho25@student.passerellesnumeriques.org>
2024-03-18 22:30:24 +0700

    add eloquen in laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,21 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
-Use App\Models\Post;
+use app\Http\Controllers\DB;
+
 class PostController extends Controller
 {
-    
+
     public function index()
     {
-     //  $allPost = Post::all();
+        /*
+       $allPost = Post::all();
+       if($allPost->count()>0){
+        foreach($allPost as $item){
+            echo $item->title;
+        }
+       }
+       */
+        //       $detailPost = Post::find(1);
+        //     dd($detailPost);
+        // $activePost = DB::table('posts')->where('status',1)->orderBy('id','DESC')->get();
+        /*
+     $activePost = Post::where('status',1)->get();
+       if($activePost->count()>0){
+        foreach($activePost as $item){
+            echo $item->title .'<br>';
+        }
+       }
+       //
+        $allPost = Post::all();
+        $activePosst=  $allPost->reject(function($post){
+            return $post->status==0;
+        });
 
-      //  dd($allPost);
+        dd($activePosst);
+      
+ $allPost = Post::all();
+    Post::chunk(2, function ($sposts){
+            foreach($sposts as  $posts ){
+                echo $posts->title .'<br>';
+            }
 
-      $post = new Post();
-      $post->title = 'Bài viết 1';
-      $post->content = 'Nội dung 1';
-     /// $post->statuts = 1;
-      $post->save();
+            echo "Kết thúc";
+    });
+  */
+        $allPost = Post::cursor();
+
+        foreach ($allPost as $item) {
+           echo $item->title . '<br>';
+        }
     }
 }
